29e0dcf0c35776e7bd3f7c5a198e13b075478dc0
shzliang99-ctrl <shzliang99@gmail.com>
2026-09-09 00:30:51 +0700

    Fix auto-reply page persistence and save logic

---
+++ app/layout.tsx
@@ -0,0 +1,7 @@
+export const metadata = {
+  title: "1 Click Boost - Ads Manager Pro",
+  description: "Manage your Facebook ads easily with 1 Click Boost",
+  icons: {
+    icon: "/logo.png",
+  },
+};
